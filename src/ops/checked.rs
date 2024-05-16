ad3d99babb9fee17e322edd9461a3d35bbfdfd3f
Murisi Tarusenga <murisit@gmail.com>
2024-05-16 11:20:51 +0200

    Stop requiring that unchecked arithmetic operators be implemented.

--- src/ops/checked.rs
+++ src/ops/checked.rs
@@ -8,7 +8,7 @@ pub trait CheckedAdd<Rhs = Self>: Sized {
 
 macro_rules! checked_impl {
     ($trait_name:ident, $method:ident, $t:ty) => {
-        impl $trait_name for $t {
+        impl $trait_name<$t> for $t {
             type Output = $t;
             #[inline]
             fn $method(self, v: $t) -> Option<$t> {
@@ -16,7 +16,23 @@ macro_rules! checked_impl {
             }
         }
 
-        impl $trait_name for &$t {
+        impl $trait_name<&$t> for $t {
+            type Output = $t;
+            #[inline]
+            fn $method(self, v: &$t) -> Option<$t> {
+                <$t>::$method(self, *v)
+            }
+        }
+
+        impl $trait_name<$t> for &$t {
+            type Output = $t;
+            #[inline]
+            fn $method(self, v: $t) -> Option<$t> {
+                <$t>::$method(*self, v)
+            }
+        }
+
+        impl $trait_name<&$t> for &$t {
             type Output = $t;
             #[inline]
             fn $method(self, v: &$t) -> Option<$t> {
